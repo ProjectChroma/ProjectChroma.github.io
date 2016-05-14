b9ac6afe588a2f75d93e6e06ea68051c9e3cc4f5
Cynthia <hiyacynth0@gmail.com>
2016-05-14 16:36:53 -0700

    Roadmap

---
+++ raw/about-game/roadmap.js
@@ -0,0 +1,24 @@
+var converter = new showdown.Converter();
+$.getJSON('https://api.github.com/repos/ProjectChroma/Chroma/milestones', {state: 'open', sort: 'completeness', direction: 'desc'}, function(milestones){
+	var container = $('<ul></ul>').attr('id', 'roadmap');
+	for(var m=0; m<milestones.length; m++){
+		var milestone = milestones[m];
+		var numClosed = milestone.closed_issues, numIssues = milestone.open_issues + milestone.closed_issues;
+		var completion = numIssues == 0 ? 0 : Math.floor(numClosed / numIssues * 100);
+		var eMilestone = $('<li></li>').addClass('milestone').appendTo(container);
+		eMilestone.append('<h2>' + milestone.title + ': ' + numClosed + ' of ' + numIssues + ' (' + completion + '%)</h2>');
+		var issueList = $('<ul></ul>').appendTo(eMilestone);
+		$.getJSON('https://api.github.com/repos/ProjectChroma/Chroma/issues', {milestone: milestone.number}, function(issues){
+			for(var i=issues.length; i>0; --i){
+				var issue = issues[i-1];
+				var eIssue = $('<li></li>').addClass('issue').toggleClass('complete', issue.state == 'closed').appendTo(issueList);
+				eIssue.append('<h3>' + issue.title + '</h3>');
+				$('<div></div>').html(converter.makeHtml(issue.body)).appendTo(eIssue);
+			}
+		}).fail(onError);
+	}
+	$('#replaceme').after(container).remove();//Add the container immediately after the placeholder, then remove the placeholder
+}).fail(onError);
+function onError(){
+	$('#replaceme').text('An error occured. Please reload the page to try again.').css('color', 'red');
+}
