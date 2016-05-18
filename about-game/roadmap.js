35739c0fadfff63afce3e61ad58d08a5e1b268de
Cynthia <hiyacynth0@gmail.com>
2016-05-17 21:44:43 -0700

    Roadmap: properly categorize issues and collapse issue descriptions by default

--- about-game/roadmap.js
+++ about-game/roadmap.js
@@ -9,26 +9,39 @@ $.getJSON('https://api.github.com/repos/ProjectChroma/Chroma/milestones', {state
 		eMilestone.append('<h2>' + milestone.title + ': ' + numClosed + ' of ' + numIssues + ' (' + completion + '%)</h2>');
 		var progress = $('<span></span>').addClass('progress-bar').appendTo(eMilestone);
 		$('<span></span>').addClass('progress').css('width', completion + '%').appendTo(progress);
-		var issueList = $('<ul></ul>').appendTo(eMilestone);
-		$.getJSON('https://api.github.com/repos/ProjectChroma/Chroma/issues', {milestone: milestone.number, state: 'all'}, function(issues){
-			for(var i=issues.length; i>0; --i){
-				var issue = issues[i-1];
-				if(issue.state == 'closed') continue;//First loop is just open issues
-				var eIssue = $('<li></li>').addClass('issue').appendTo(issueList);
-				eIssue.append('<h3>' + issue.title + '</h3>');
-				$('<div></div>').html(converter.makeHtml(issue.body)).appendTo(eIssue);
-			}
-			for(var i=issues.length; i>0; --i){
-				var issue = issues[i-1];
-				if(issue.state == 'open') continue;
-				var eIssue = $('<li></li>').addClass('issue').addClass('complete').appendTo(issueList);
-				eIssue.append('<h3>' + issue.title + '</h3>');
-				$('<div></div>').html(converter.makeHtml(issue.body)).appendTo(eIssue);
-			}
-		}).fail(onError);
+		eMilestone.append('<ul></ul>');//Issue list
 	}
-	$('#replaceme').after(container).remove();//Add the container immediately after the placeholder, then remove the placeholder
+	$('#replaceme').before(container).remove();
 }).fail(onError);
+$.getJSON('https://api.github.com/repos/ProjectChroma/Chroma/issues', {milestone: '*', state: 'all'}, function(issues){//Retrieve all issues attached to any milestone
+	for(var i=0; i<issues.length; ++i){
+		if(issues[i].state == 'open') processIssue(issues[i]);
+	}
+	for(var i=0; i<issues.length; ++i){
+		if(issues[i].state == 'closed') processIssue(issues[i]);
+	}
+});
+function processIssue(issue){
+	var eIssue = $('<li></li>').addClass('issue');
+	var btnExpand = $('<button>+</button>').addClass('expand').on('mousedown', function(){
+		$(this).parent().siblings().css('display', 'block');
+	});
+	var btnCollapse = $('<button>-</button>').addClass('collapse').on('mousedown', function(){
+		$(this).parent().siblings().css('display', 'none');
+	});
+	$('<h3>' + issue.title + '</h3>').append(btnExpand).append(btnCollapse).appendTo(eIssue);
+	if(issue.state == 'closed') eIssue.addClass('complete');
+	else if(issue.assignee != null){
+		eIssue.addClass('active');
+		$('<h5>Assigned to: </h5>').addClass('assignee')
+		.append($('<img/>').attr('src', issue.assignee.avatar_url).addClass('assignee-avatar'))
+		.append($('<a>' + issue.assignee.login + '</a>').attr('href', issue.assignee.html_url).attr('target', '_blank'))
+		.appendTo(eIssue);
+	}
+	$('<div></div>').html(converter.makeHtml(issue.body)).appendTo(eIssue);
+	var eMilestone = $('#roadmap').children().eq(issue.milestone.number-1);//Find the element from the issue's milestone number
+	eMilestone.children('ul').append(eIssue);//Add issue to milestone's issue list
+}
 function onError(){
-	$('#replaceme').text('An error occured. Please reload the page to try again.').css('color', 'red');
+	$('#replaceme, #roadmap').text('An error occured. Please reload the page to try again.').css('color', 'red');
 }
