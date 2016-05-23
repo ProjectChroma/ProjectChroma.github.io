9c17f6035a8d7dc93c9f1ccaa395beb50b472b15
Cynthia <hiyacynth0@gmail.com>
2016-05-23 14:03:39 -0700

    Jill bio, new screenshots, fix image-wrapper stuff

--- res/image-wrapper.js
+++ res/image-wrapper.js
@@ -1,24 +1,23 @@
-var wall = new Freewall('#image-grid');
-wall.fitWidth();
-
-var grid, bkg, img;
+var bkg, img;
 $(function(){
-	grid = $('#image-grid');
 	bkg = $('#modal-bkg');
 	img = $('#modal-img');
-})
-$('body').on('mousedown', hideModal);
-$('#image-grid > img').on('mousedown', function(event){
-	showModal($(this).index(), $(this).attr('src'));
-	return false;//Prevent bubble up to body
+	$('body').on('mousedown', hideModal);
+	$('.image-wrapper').attr('data-index', function(i){return i});//Add grid IDs to each image wrapper
+	$('.image-wrapper > img').on('mousedown', function(event){
+		showModal($(this).index(), $(this).parent().attr('data-index'), $(this).attr('src'));
+		return false;//Prevent bubble up to body
+	});
 });
 $(window).on('keydown', function(event){
 	if(event.keyCode == 37){//Left
+		var grid = $('.image-wrapper[data-index="' + img.attr('data-gridindex') + '"]');
 		var ind = Number(img.attr('data-index'));
 		var newInd = ind == 0 ? grid.children().length - 1 : ind - 1;
 		img.attr('data-index', newInd);
 		img.attr('src', grid.children().eq(newInd).attr('src'));
 	}else if(event.keyCode == 39){//Right
+		var grid = $('.image-wrapper[data-index="' + img.attr('data-gridindex') + '"]');
 		var ind = Number(img.attr('data-index'));
 		var newInd = (ind + 1) % grid.children().length;
 		img.attr('data-index', newInd);
@@ -26,8 +25,8 @@ $(window).on('keydown', function(event){
 	}else if(event.keyCode == 27) hideModal();//Escape
 });
 
-function showModal(index, src){
-	img.attr('src', src).attr('data-index', index);
+function showModal(index, gridIndex, src){
+	img.attr('src', src).attr('data-index', index).attr('data-gridindex', gridIndex);
 	bkg.css('z-index', 2).animate({
 		'opacity': 1
 	}, 1000);
